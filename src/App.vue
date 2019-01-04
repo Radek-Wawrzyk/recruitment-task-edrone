48be965b3b70250b0b74f2393b79da0fbda6b059
Radek <radek511@op.pl>
2019-01-04 17:04:05 +0000

    Add - Simply view of recipes and new font

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
     <Navigation />
+    <main class="main">
+      <el-row :gutter="30">
+        <el-col :md="8" :lg="6" v-for="(item, index) in items" :key="index" >
+          <el-card :body-style="{ padding: '0px' }">
+            <img :src="item.img" class="image">
+            <div style="padding: 14px;">
+              <h3>{{item.name}}</h3>
+            </div>
+          </el-card>
+        </el-col>
+      </el-row>
+    </main>
   </div>
 </template>
 
@@ -11,6 +23,45 @@ export default {
   name: 'App',
   components: {
     Navigation  
-  }
+  },
+  data: () => ({
+    items: [
+      {
+        id: 0,
+        name: 'Burger 0',
+        img: 'https://element.eleme.io/static/hamburger.50e4091.png'
+      },
+      {
+        id: 1,
+        name: 'Burger 1',
+        img: 'https://element.eleme.io/static/hamburger.50e4091.png'
+      },
+      {
+        id: 2,
+        name: 'Burger 2',
+        img: 'https://element.eleme.io/static/hamburger.50e4091.png'
+      },
+      {
+        id: 3,
+        name: 'Burger 3',
+        img: 'https://element.eleme.io/static/hamburger.50e4091.png'
+      },
+      {
+        id: 4,
+        name: 'Burger 4',
+        img: 'https://element.eleme.io/static/hamburger.50e4091.png'
+      },
+      {
+        id: 5,
+        name: 'Burger 5',
+        img: 'https://element.eleme.io/static/hamburger.50e4091.png'
+      },
+      {
+        id: 6,
+        name: 'Burger 6',
+        img: 'https://element.eleme.io/static/hamburger.50e4091.png'
+      }
+    ]
+  })
 };
 </script>
