6fd89e0fb71aba02d60cea8cf381bc8c28b45432
Radek <radek511@op.pl>
2019-01-08 20:22:34 +0000

    Fix - Recipes ingredients

--- src/views/OpenRecipe/OpenRecipe.vue
+++ src/views/OpenRecipe/OpenRecipe.vue
@@ -1,17 +1,25 @@
 <template>
-  <el-dialog :title="recipe.strMeal" :visible.sync="dialogVisible" class="dialog" :before-close="close" style="width: 100%;">
+  <el-dialog
+    :title="recipe.strMeal"
+    :visible.sync="dialogVisible"
+    class="dialog"
+    :before-close="close"
+    style="width: 100%;"
+  >
     <div class="recipe">
       <div class="recipe-info">
         <h2 class="recipe-info-title">Instruction</h2>
         <p class="recipe-info-description">{{recipe.strInstructions}}</p>
         <ul class="recipe-info-list">
-          <li class="recipe-info-list-item" v-for="(value, key) in recipe.ingredients" :key="key">
-            {{value}}
-          </li>
+          <li v-for="(ingredient, index) in recipe.ingredients" :key="index">{{ingredient.name}} - {{ingredient.value || 'More or less'}}</li>
         </ul>
         <youtube :video-id="recipe.youTubeId" v-if="recipe.youTubeId"></youtube>
         <footer class="recipe-info-link" v-if="recipe.strSource">
-          <a :href="recipe.strSource" :aria-label="recipe.strMeal" :title="recipe.strMeal">Source: {{recipe.strSource}}</a>
+          <a
+            :href="recipe.strSource"
+            :aria-label="recipe.strMeal"
+            :title="recipe.strMeal"
+          >Source: {{recipe.strSource}}</a>
         </footer>
       </div>
     </div>
@@ -22,12 +30,9 @@
 </template>
 
 <script>
-
 export default {
-  name: 'OpenRecipe',
-  props: [
-    'id'
-  ],
+  name: "OpenRecipe",
+  props: ["id"],
   data: () => ({
     dialogVisible: true
   }),
@@ -35,49 +40,46 @@ export default {
     recipe() {
       const recipe = this.$store.getters.openedRecipe(this.id)[0];
 
-      //Delete blank objects
+      //Delete blank objectsa 
       for (const [key, value] of Object.entries(recipe)) {
         if (value === "") {
           delete recipe[key];
         }
       }
 
-      // //Search for ingredients keys
-      // const ingredientsKeys = Object.keys(recipe).filter(ingredient =>
-      //   ingredient.includes("strIngredient")
-      // );
+      //Search for ingredients keys
+      const ingredientsKeys = Object.keys(recipe).filter(ingredient =>
+        ingredient.includes("strIngredient")
+      );
 
-      // console.log(ingredientsKeys);
+      console.log(ingredientsKeys);
 
-      // //Make array from ingredients and measures
-      // const ingredients = ingredientsKeys.reduce((reduced, itemKey) => {
-      //   const itemIndex = itemKey.slice(13);
+      //Make array from ingredients and measures
+      const ingredients = ingredientsKeys.reduce((reduced, itemKey) => {
+        const itemIndex = itemKey.slice(13);
 
-      //   reduced.push({[recipe]: recipe['strMeasure' + itemIndex]});
-      //   return reduced;
-      // }, []);
-
-        const ingredientsKeys = Object.keys(recipe).filter(ingredient =>
-          ingredient.includes("strIngredient")
+        reduced.push(
+          Object.assign(
+            {},
+            { name: recipe[itemKey], value : recipe["strMeasure" + itemIndex] }
+          )
         );
+        return reduced;
+      }, []);
 
-        const ingredients = ingredientsKeys.reduce((reduced, itemKey) => {
-          const itemIndex = itemKey.slice(-1);
-          reduced.push(Object.assign({}, { [recipe[itemKey]]: recipe['strMeasure' + itemIndex] }));
-          return reduced;
-        }, []);
+      Object.assign(recipe, { ingredients });
 
-      recipe.ingredients = ingredients;
+      console.log(recipe.ingredients);
 
       //Get YouTube movie ID
 
       if (recipe.strYoutube) {
-        const splitString = recipe.strYoutube.split('watch?v=');
+        const splitString = recipe.strYoutube.split("watch?v=");
         const id = splitString[1];
-        
+
         recipe.youTubeId = id;
       }
-      
+
       return recipe;
     }
   },
